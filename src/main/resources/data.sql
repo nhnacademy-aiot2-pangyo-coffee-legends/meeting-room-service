823d663ee2d0e74393fac26aceae693aeb2e6839
LEMON4DE <111743489+LEMON4DE@users.noreply.github.com>
2025-05-20 16:46:32 +0900

    deploy : 배포 환경 설정 및 secret keys 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO meeting_rooms (meeting_room_capacity, meeting_room_name) VALUES(10, '회의실 A');
-INSERT INTO meeting_rooms (meeting_room_capacity, meeting_room_name) VALUES(15, '회의실 B');
-INSERT INTO meeting_rooms (meeting_room_capacity, meeting_room_name) VALUES(7, '회의실 C');
-INSERT INTO meeting_rooms (meeting_room_capacity, meeting_room_name) VALUES(12, '회의실 D');
+INSERT INTO meeting_rooms (meeting_room_name, meeting_room_capacity) VALUES('회의실 A', 10);
+INSERT INTO meeting_rooms (meeting_room_name, meeting_room_capacity) VALUES('회의실 B', 15);
+INSERT INTO meeting_rooms (meeting_room_name, meeting_room_capacity) VALUES('회의실 C', 7);
+INSERT INTO meeting_rooms (meeting_room_name, meeting_room_capacity) VALUES('회의실 D', 12);
