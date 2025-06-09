6e84ec31ef92ad3c9a9ec201046fb9a8ce5dd6aa
ur <dorosi150@gmail.com>
2025-06-10 08:46:36 +0900

    feat : 기자재 DDL 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,18 @@ CREATE TABLE meeting_rooms (
 );
 
 CREATE TABLE equipments (
-    equipment_no BIGINT AUTO_INCREMENT PRIMARY KEY,
-    equipment_type VARCHAR(50) NOT NULL,
-    equipment_name VARCHAR(50) NOT NULL
+                            equipment_no BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            equipment_type VARCHAR(50) NOT NULL,
+                            equipment_name VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE `meeting_room_equipment` (
+                                          `meeting_room_equipment_no` BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                          `meeting_room_no`	BIGINT	NOT NULL,
+                                          `equipment_no`	BIGINT	NOT NULL
+);
+
+
+ALTER TABLE `meeting_room_equipment`
+    ADD CONSTRAINT `FK_MEETING_ROOM` FOREIGN KEY (`meeting_room_no`) REFERENCES meeting_rooms(`meeting_room_no`),
+    ADD CONSTRAINT `FK_EQUIPMENT` FOREIGN KEY (`equipment_no`) REFERENCES equipments(`equipment_no`);
